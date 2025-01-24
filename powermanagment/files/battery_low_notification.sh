0d67105b0dcd78eefc4a4f56a48c0fa52e45c515
Lars Janssen <lars.e.janssen@gmail.com>
2025-01-24 12:15:54 +0100

    powermanagment

---
+++ powermanagment/files/battery_low_notification.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+send-notification ()
+{
+    XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send "$1" "$2" $3 $4 
+}
+
+if ! acpi | grep 'Charging' --quiet
+then
+    battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
+    if [ $battery_level -le $1 ]
+    then
+        urgent=""
+        status="low"
+        if [ $2 == "critical" ]; then
+            status="critical!"
+            urgent="--urgency=critical --expire-time=30000"
+        fi
+        send-notification "Battery ${status}" "Battery level is ${battery_level}%!" $urgent
+    fi
+fi

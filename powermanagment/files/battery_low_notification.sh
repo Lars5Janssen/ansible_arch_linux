465dfa11cabf4c6c6f5e345766f245767d946972
Lars Janssen <lars.e.janssen@gmail.com>
2025-01-24 12:48:30 +0100

    battery_low_notification dimms display_brightness on critical

--- powermanagment/files/battery_low_notification.sh
+++ powermanagment/files/battery_low_notification.sh
@@ -7,11 +7,16 @@ send-notification ()
 if ! acpi | grep 'Charging' --quiet
 then
     battery_level=`acpi -b | grep -P -o '[0-9]+(?=%)'`
-    if [ $battery_level -le $1 ]
-    then
+    if [ $battery_level -le $1 ]; then
         urgent=""
         status="low"
         if [[ $2 == "critical" ]]; then
+            display_brightness="$(brightnessctl | grep '%' | awk '{ print $4 }' | sed 's/(//' | sed 's/%)//')"
+            if [ $display_brightness -gt 31 ]; then
+                if [ $display_brightness -gt 29 ]; then
+                    brightnessctl set 30% 
+                fi
+            fi
             status="critical!"
             urgent="--urgency=critical --expire-time=30000"
         fi

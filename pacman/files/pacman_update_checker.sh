c3829309fd5cc249e8061686e3a44a9a87f5bbbf
Lars Janssen <lars.e.janssen@gmail.com>
2025-01-27 11:37:17 +0100

    pacman cron

--- pacman/files/pacman_update_checker.sh
+++ pacman/files/pacman_update_checker.sh
@@ -4,6 +4,10 @@ send-notification ()
     XDG_RUNTIME_DIR=/run/user/$(id -u) notify-send "$1" "$2" $3 $4 
 }
 
+min_wait=0
+max_wait=59
+sleep $((min_wait+RANDOM % (max_wait-min_wait)))
+
 if checkupdates
 then
     send-notification "Pacman Updates available" --expire-time=1800000
